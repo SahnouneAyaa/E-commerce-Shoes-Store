2f419d05ac395d97e0267fa2c837a0afaf55a854
SahnouneAyaa <sahnouneaya18@gmail.com>
2024-04-14 00:43:51 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Specify the output directory
+    assetsInlineLimit: 0, // Disable asset size limit (to inline all assets)
+    cssCodeSplit: true, // Enable CSS code splitting
+    sourcemap: true, // Generate source maps
+    minify: 'terser', // Minify code using Terser
+  },
+});
